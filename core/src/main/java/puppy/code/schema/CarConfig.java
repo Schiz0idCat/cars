506fe65998cbfb0a82bfbaab85a89abc53b5909b
nico0417 <nicolaslei18@gmail.com>
2025-10-30 01:53:56 -0300

    forgot: resize pngs to 96x124 and now screen is resizable

--- core/src/main/java/puppy/code/schema/CarConfig.java
+++ core/src/main/java/puppy/code/schema/CarConfig.java
@@ -4,7 +4,7 @@ public class CarConfig {
     public static final int WIDTH = 94;
     public static final int HEIGHT = 128;
     public static final int START_LIVES = 3;
-    public static final int SPEED = 1500; // pixeles por segundo
+    public static final int SPEED = 400; // pixeles por segundo
     public static final int DAMAGE_ANIMATION_TICKS = 50; // tiempo de freeze cuando recibe daño
     public static final int START_Y_POSITION = 20;
 }
