223a99b9c39dad423bb8616f3b62fc8e99dd03ca
Agustín <agustin.guzman.v@proton.me>
2025-10-30 11:32:51 -0300

    better hitbox

--- core/src/main/java/puppy/code/schema/CarConfig.java
+++ core/src/main/java/puppy/code/schema/CarConfig.java
@@ -1,8 +1,8 @@
 package puppy.code.schema;
 
 public class CarConfig {
-    public static final int WIDTH = 94;
-    public static final int HEIGHT = 128;
+    public static final int WIDTH = 80;
+    public static final int HEIGHT = 115;
     public static final int START_LIVES = 3;
     public static final int SPEED = 400; // pixeles por segundo
     public static final int DAMAGE_ANIMATION_TICKS = 50; // tiempo de freeze cuando recibe daño
