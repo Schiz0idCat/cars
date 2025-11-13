cd65b2fd0d3dbc897a9d3f1fa825d43b95d59519
nico0417 <nicolaslei18@gmail.com>
2025-11-13 19:56:57 -0300

    create AssetManager for singleton

---
+++ core/src/main/java/puppy/code/AssetManager.java
@@ -0,0 +1,9 @@
+package puppy.code;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Disposable;
+import java.util.HashMap;
+import java.util.Map;
